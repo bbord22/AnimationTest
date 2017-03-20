59e864d8508186d5cf04a15953f42be4474aaf16
bbord22 <bmucle@yahoo.com>
2017-03-20 17:09:54 -0500

    Added Possible Hook v2

--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -1,40 +1,97 @@
-﻿using System.Collections;
+﻿// To make this script work, all of the Ground objects must have the tag "Ground"
+// and all of the Wall objects must have the tag "Wall". Also, the Player object
+// must have a rigidbody2D attached.
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+	private float moveSpeed;
+	private Vector3 jumpHeight;
+	private Vector3 slideHeight;
+	private Rigidbody2D rb;
+	private bool canJump;
+	private bool isWallJumping;
+	private bool isWallSliding;
+	private float oldHeight;
+	private float currentHeight;
 
-	private Animator animator;
-
-	// Use this for initialization
-	void Start()
+	void Start ()
 	{
-		animator = this.GetComponent<Animator>();
+		rb = gameObject.GetComponent<Rigidbody2D> ();
+		jumpHeight = new Vector3 (0f, 10f, 0f);
+		slideHeight = new Vector3 (0f, 10f, 0f);
+		moveSpeed = 5f;
+		canJump = true;
+		isWallJumping = false;
+		isWallSliding = false;
+		currentHeight = gameObject.transform.position.y;
+		oldHeight = currentHeight;
 	}
 
-	// Update is called once per frame
-	void Update()
+	void Update ()
 	{
+		if (isWallJumping == true) {
+			moveSpeed = 4f; // affects the distance that the player can jump from a wall
+		} else {
+			moveSpeed = 5f; // affects the speed that the player moves around
+		}
+		transform.rotation = Quaternion.Euler (0, 0, 0); // stops rotation
+		if (Input.GetKey ("a")) {
+			transform.Translate (Vector3.left * moveSpeed * Time.deltaTime); // move to the left
+		}
+		if (Input.GetKey ("d")) {
+			transform.Translate (Vector3.right * moveSpeed * Time.deltaTime); // move to the right
+		}
+		if (Input.GetKey ("w") && canJump == true) {
+			rb.AddForce (jumpHeight, ForceMode2D.Impulse); // jump
+			canJump = false;
+		}
+		currentHeight = gameObject.transform.position.y; // did this so the jump won't look floaty
+		if (currentHeight < oldHeight) { // if player is falling gravity is less
+			rb.gravityScale = 1;
+		} else { // if player is jumping gravity is more
+			rb.gravityScale = 2;
+		}
+		oldHeight = currentHeight;
+	}
 
-		var vertical = Input.GetAxis("Vertical");
-		var horizontal = Input.GetAxis("Horizontal");
+	void OnCollisionEnter2D (Collision2D other)
+	{
+		rb.gravityScale = 1;
+		isWallJumping = false;
+		if (other.gameObject.tag == "Ground") {
+			canJump = true;
+			isWallSliding = false;
+			Debug.Log ("Touched Ground");
+		}
+		if (other.gameObject.tag == "Wall" && isWallSliding == false) {
+			rb.AddForce (slideHeight, ForceMode2D.Impulse);
+			canJump = false;
+			isWallSliding = true;
+			Debug.Log ("Wall Slide");
+		}
+	}
 
-		if (vertical > 0)
-		{
-			animator.SetInteger("Direction", 2);
+	void OnCollisionStay2D (Collision2D other)
+	{
+		if (other.gameObject.tag == "Wall" && Input.GetKey ("w") && isWallSliding == false) {
+			isWallSliding = true;
+			rb.gravityScale = 1;
+			rb.AddForce (slideHeight, ForceMode2D.Impulse);
 		}
-		else if (vertical < 0)
-		{
-			animator.SetInteger("Direction", 0);
-		}
-		else if (horizontal > 0)
-		{
-			animator.SetInteger("Direction", 3);
-		}
-		else if (horizontal < 0)
-		{
-			animator.SetInteger("Direction", 1);
+	}
+
+	void OnCollisionExit2D (Collision2D other)
+	{
+		isWallSliding = false;
+		canJump = false;
+		Debug.Log ("Left the " + other.gameObject.tag);
+		if (other.gameObject.tag == "Wall") {
+			isWallJumping = true;
+			Debug.Log ("Wall Jump");
 		}
 	}
 }
